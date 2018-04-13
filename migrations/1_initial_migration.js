d4e77c35190182aca2b34dee5334d372483f2b0d
WWWillems <lander.willems@gmail.com>
2018-04-13 16:05:51 +0200

    Updates initial migration to reflect correct constructor.

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,9 @@
 var Splitter = artifacts.require("./Splitter.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(Splitter);
+module.exports = function(deployer, network, accounts) {
+  deployer.deploy(
+      Splitter,
+      accounts[1],
+      accounts[2],
+      { from: accounts[0], gas:1000000});
 };
